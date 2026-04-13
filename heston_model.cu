a752d3613ab7d1adb7f391befe59dce2f316c269
Rayane Troudi <rayanetroudi1@gmail.com>
2026-04-13 19:31:30 +0200

    Implement testCuda function to check if CUDA is correctly call

--- heston_model.cu
+++ heston_model.cu
@@ -3,3 +3,11 @@
 #include <math.h>
 #include <curand_kernel.h>
 
+// Function that catches the error
+void testCUDA(cudaError_t error, const char* file, int line) {
+    if (error != cudaSuccess) {
+        printf("There is an error in file %s at line %d\n", file, line);
+        exit(EXIT_FAILURE);
+    }
+}
+
