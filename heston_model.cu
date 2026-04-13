811c29800a14354f626617803f1283b1e3b13d86
Rayane Troudi <rayanetroudi1@gmail.com>
2026-04-13 21:26:51 +0200

    building the main to compute a first iteration of the MC_Heston_Model

--- heston_model.cu
+++ heston_model.cu
@@ -10,6 +10,8 @@ void testCUDA(cudaError_t error, const char* file, int line) {
         exit(EXIT_FAILURE);
     }
 }
+#define testCUDA(error) (testCUDA(error, __FILE__, __LINE__))
+
 
 // Init the seed for each thread that compute a path price
 __global__ void init_curand_state_k(curandState* state) {
@@ -34,7 +36,7 @@ __global__ void MC_heston_model(float kappa, float theta, float sigma, float r, 
     float V = 0.1f; // init of the first value of the vol
 
 
-
+    
     // ___ SIMULATION BLOCK ___
     for (int i = 0; i < N; i++)
     {
@@ -61,15 +63,67 @@ int main(void){
     float S0 = 50.0f; 
     float K = S0; // at the money
 
-    int N = 1000;
-    float dt = 1.0f / (float) N;
+    int N = 100;
+    float dt = 1.0f / 1000.0f ;
 
     float sigma = 0.3f;
     float kappa = 0.5f;
     float theta = 0.1f;
+    float r = 0.024f;
+    float rho = 1.0;
 
-    float *payoffGPU;
+    float sum = 0.0f;
+    float sum2 = 0.0f;
+
+    float *payoffGPU, *payoffCPU;
+    printf("%s \n","ntm");
+
+    // init of the array that contains all the payoff from each path generated
+    payoffCPU = (float*)malloc(n * sizeof(float));
+    testCUDA(cudaMalloc(&payoffGPU, n * sizeof(float)));
+
+    // init of the array that will contain the state of each thread
+    curandState *states;
+    testCUDA(cudaMalloc(&states, n * sizeof(curandState)));
+
+    //init of the state of each thread
+    init_curand_state_k<<<NB, NTPB>>>(states);
+    testCUDA(cudaGetLastError());
+    testCUDA(cudaDeviceSynchronize());
+
+    // init of the timer
+    float Tim;
+    cudaEvent_t start, stop;
+    testCUDA(cudaEventCreate(&start));
+    testCUDA(cudaEventCreate(&stop));
+    testCUDA(cudaEventRecord(start, 0));
+
+    // launch the kernel that compute the payoff of each path at maturity
+    MC_heston_model<<<NB,NTPB>>>(kappa,theta,sigma,r,rho,dt,N,K,S0,states,payoffGPU);
+    testCUDA(cudaGetLastError());
+  
+    // stop the timer
+    testCUDA(cudaEventRecord(stop, 0));
+    testCUDA(cudaEventSynchronize(stop));
+    testCUDA(cudaEventElapsedTime(&Tim, start, stop));
+    testCUDA(cudaEventDestroy(start));
+    testCUDA(cudaEventDestroy(stop));
+
+    // copy the payoff of each path from the device to the host
+    testCUDA(cudaMemcpy(payoffCPU, payoffGPU, n * sizeof(float), cudaMemcpyDeviceToHost));
     
+    // Reduction performed on the host
+    for (int i = 0; i < n; i++) {
+        sum += payoffCPU[i] / n;
+        sum2 += payoffCPU[i] * payoffCPU[i] / n;
+    }
+
+    printf("The estimated price is equal to %f\n", sum);
+
+    // free the memory
+    free(payoffCPU);
+    testCUDA(cudaFree(payoffGPU));
+    testCUDA(cudaFree(states));
 
     return 0;
 }
